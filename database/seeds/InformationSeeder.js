2547d9910d0b518164275c12359f5a4097255edb
Matheus Marques <matheusml25@hotmail.com>
2020-11-10 11:11:48 -0300

    update informations seed

--- database/seeds/InformationSeeder.js
+++ database/seeds/InformationSeeder.js
@@ -14,27 +14,27 @@ const Information = use('App/Models/Information')
 
 class InformationSeeder {
   async run () {
-    await Information.create({ level: 1, description: 'description 1'});
-    await Information.create({ level: 1, description: 'description 2'});
-    await Information.create({ level: 1, description: 'description 3'});
-    await Information.create({ level: 1, description: 'description 4'});
-    await Information.create({ level: 1, description: 'description 5'});
-    await Information.create({ level: 1, description: 'description 6'});
-    await Information.create({ level: 2, description: 'description 7'});
-    await Information.create({ level: 2, description: 'description 8'});
-    await Information.create({ level: 2, description: 'description 9'});
-    await Information.create({ level: 2, description: 'description 10'});
-    await Information.create({ level: 2, description: 'description 11'});
-    await Information.create({ level: 2, description: 'description 12'});
-    await Information.create({ level: 2, description: 'description 13'});
-    await Information.create({ level: 2, description: 'description 14'});
-    await Information.create({ level: 2, description: 'description 15'});
-    await Information.create({ level: 2, description: 'description 16'});
-    await Information.create({ level: 3, description: 'description 17'});
-    await Information.create({ level: 3, description: 'description 18'});
-    await Information.create({ level: 3, description: 'description 19'});
-    await Information.create({ level: 3, description: 'description 20'});
-    await Information.create({ level: 3, description: 'description 21'});
+    await Information.create({ level: 1, description: 'Nulla quis justo nec mauris.'});
+    await Information.create({ level: 1, description: 'Fusce tristique id sapien at.'});
+    await Information.create({ level: 1, description: 'Orci varius natoque penatibus et.'});
+    await Information.create({ level: 1, description: 'In hac habitasse platea dictumst.'});
+    await Information.create({ level: 1, description: 'Aliquam ut rutrum leo. Vivamus.'});
+    await Information.create({ level: 1, description: 'Sed non commodo felis. Etiam.'});
+    await Information.create({ level: 2, description: 'Fusce malesuada varius justo in.'});
+    await Information.create({ level: 2, description: 'Duis id mi ac nisi.'});
+    await Information.create({ level: 2, description: 'Praesent non ornare ex. Aliquam.'});
+    await Information.create({ level: 2, description: 'Vestibulum ex interdum metus, quis.'});
+    await Information.create({ level: 2, description: 'Ut nisl nisl, interdum vel.'});
+    await Information.create({ level: 2, description: 'Sed vulputate lectus ut magna.'});
+    await Information.create({ level: 2, description: 'Sed vulputate molestie neque in.'});
+    await Information.create({ level: 2, description: 'Nunc porttitor mattis elit, id.'});
+    await Information.create({ level: 2, description: 'Phasellus et ante ante. Nam.'});
+    await Information.create({ level: 2, description: 'Vestibulum eget tempus risus. elit.'});
+    await Information.create({ level: 3, description: 'In a lobortis libero. Vestibulum.'});
+    await Information.create({ level: 3, description: 'Mauris sit amet laoreet nunc.'});
+    await Information.create({ level: 3, description: 'Mauris et auctor eros, a.'});
+    await Information.create({ level: 3, description: 'Etiam eget ex sed eros.'});
+    await Information.create({ level: 3, description: 'Nullam vestibulum mi et augue.'});
   }
 }
 
